27665301fad6bf74ef55f00c9dd717c9da2963d6
Ian Norton <inb@ncipher.com>
2009-02-28 21:35:23 +0000

    This time really add doxygen markup

--- xrlist.c
+++ xrlist.c
@@ -1,6 +1,50 @@
 #include <assert.h>
 #include "xrlist.h"
 
+/**
+ * \mainpage xrlist Documentation Index
+ *
+ * \section intro_sec Introduction
+ *
+ * xrlist is a simple dynamic array implemented as a double linked list. It
+ * provides functions for the standard push,pop,shift and unshift operations on
+ * the array.
+ *
+ * \see xrlist.h
+ *
+ * \section license_sec BSD Public License
+ *
+Copyright (c) 2009, Ian Norton
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    + Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.  
+
+    + Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    + Neither the name of xrlist nor the names of its contributors may be used
+      to endorse or promote products derived from this software without
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ *
+ */
+
 xr_list_t * xrlist_new( void )
 {
   xr_list_t * list = malloc( 1 * sizeof(xr_list_t) );
--- xrlist.h
+++ xrlist.h
@@ -4,34 +4,108 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct xr_list_item_t {
-  struct xr_list_item_t * next;
-  struct xr_list_item_t * prev;
-  struct xr_list_t      * list;
-  void * object;
-};
+/**
+ * \class
+ *
+ *
+ *
+ */
 
-struct xr_list_t {
-  struct xr_list_item_t * head;
-  struct xr_list_item_t * tail;
-  size_t count;
-};
+/**
+ * An xrlist item. 
+ *
+ * Simply contains a pointer to the parent list, a pointer to the
+ * actual list payload and pointers to the next and preceeding list items in
+ * this list. You need not allocate these directly.
+ */ 
+typedef struct xr_list_item_t {
+  struct xr_list_item_t * next; /** The next item in this list, NULL if the first item */
+  struct xr_list_item_t * prev; /** The previous item in this list, NULL if the last item */
+  struct xr_list_t      * list; /** The parent list structure */
+  void * object;                /** Pointer to the list item contents */
+} xr_list_item_t;
 
-typedef struct xr_list_item_t xr_list_item_t;
-typedef struct xr_list_t      xr_list_t;
+/**
+ * The actual xrlist structure. 
+ *
+ * This is used to identify the entire list aswell as track it's contents.
+ */
+typedef struct xr_list_t {
+  struct xr_list_item_t * head; /** The first item in this list, NULL if no items are held */
+  struct xr_list_item_t * tail; /** The last item in this list, NULL if no items are held */
+  size_t count; /** The number of items in this list */
+} xr_list_t;
 
-extern xr_list_t *      xrlist_new( void );
-extern xr_list_item_t * xrlist_new_item ( void );
+/** Allocate a new xrlist object
+ *
+ * \return pointer to a new xrlist object
+ */
+xr_list_t *      xrlist_new( void );
 
-extern void             xrlist_free( xr_list_t * list );
-extern void             xrlist_free_items( xr_list_t * list);
-extern void             xrlist_free_item( xr_list_item_t * item );
+/* Private 
+ * Allocate a new xrlist item
+ *
+ * \return pointer to a new xrlist item 
+ */
+xr_list_item_t * xrlist_new_item ( void );
 
-extern xr_list_item_t * xrlist_push( xr_list_t * list, void * object );
-extern void *           xrlist_pop( xr_list_t * list );
+/** Free an xrlist object but do not free it's items
+ *
+ * \param list Pointer to the xrlist to free
+ */
+void             xrlist_free( xr_list_t * list );
 
-extern xr_list_item_t * xrlist_unshift( xr_list_t * list, void * object );
-extern void *           xrlist_shift( xr_list_t * list );
+/** Free an xrlist object including all it's list items, 
+ * but do not free the object pointer in each item 
+ *
+ * \param list Pointer to the xrlist to free
+ * */
+void             xrlist_free_items( xr_list_t * list);
+
+/* Private
+ * Free a single xrlist item
+ *
+ * \param item Pointer to the xrlist item to free
+ */
+void             xrlist_free_item( xr_list_item_t * item );
+
+/**
+ * Add a new item to the end of list containing a pointer to object
+ *
+ * \param list Pointer to the xrlist to add an item to
+ * \param object Pointer to the memory/object to be stored in the list
+ *
+ * \return A pointer to the xrlist item now containing object
+ */
+xr_list_item_t * xrlist_push( xr_list_t * list, void * object );
+
+/**
+ * Remove the last item in list 
+ *
+ * \param list Pointer to the xrlist to remove an item from
+ *
+ * \return A pointer to the memory/object stored in the list item just removed
+ */
+void *           xrlist_pop( xr_list_t * list );
+
+/**
+ * Add a new item to the start of list containing a pointer to object
+ *
+ * \param list Pointer to the xrlist to add an item to
+ * \param object Pointer to the memory/object to be stored in the list
+ *
+ * \return A pointer to the xrlist item now containing object
+ */
+xr_list_item_t * xrlist_unshift( xr_list_t * list, void * object );
+
+/**
+ * Remove the first itemm in list
+ *
+ * \param list Pointer to the xrlist to remove an item from
+ *
+ * \return A pointer to the memory/object stored in the list item just removed
+ */
+void *           xrlist_shift( xr_list_t * list );
 
 
 
